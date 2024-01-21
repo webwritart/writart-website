259070dc593856f0ffbff127657ba07830f4cd6a
Shwetabh <webwritat@gmail.com>
2024-01-21 11:04:02 +0530

    change create_app to if __name__ == __main__:

--- app.py
+++ app.py
@@ -1,42 +1,42 @@
 import os
+
 from flask import Flask
-from extensions import db, mail, login_manager
+
+from extensions import mail, login_manager, db
+from models.member import Member, member_role, member_workshop, Workshop, Role
 from routes.account import account
 from routes.main import main
-from routes.school import school
-from models.member import *
-from routes.payment import payment
 from routes.manager import manager
+from routes.payment import payment
+from routes.school import school
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('APP_SECRET')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+app.config["MAIL_SERVER"] = 'smtp.gmail.com'
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USERNAME"] = os.environ.get('MAIL_USERNAME')
+app.config["MAIL_PASSWORD"] = os.environ.get('MAIL_PASSWORD')
+app.config["MAIL_USE_SSL"] = True
+mail.init_app(app)
+db.init_app(app)
+login_manager.init_app(app)
+
+app.register_blueprint(main, url_prefix='/')
+app.register_blueprint(account, url_prefix='/account')
+app.register_blueprint(school, url_prefix='/school')
+app.register_blueprint(payment, url_prefix='/payment')
+app.register_blueprint(manager, url_prefix='/manager')
 
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = os.environ.get('APP_SECRET')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
-    app.config["MAIL_SERVER"] = 'smtp.gmail.com'
-    app.config["MAIL_PORT"] = 465
-    app.config["MAIL_USERNAME"] = os.environ.get('MAIL_USERNAME')
-    app.config["MAIL_PASSWORD"] = os.environ.get('MAIL_PASSWORD')
-    app.config["MAIL_USE_SSL"] = True
-    mail.init_app(app)
-    db.init_app(app)
-    login_manager.init_app(app)
-
-    app.register_blueprint(main, url_prefix='/')
-    app.register_blueprint(account, url_prefix='/account')
-    app.register_blueprint(school, url_prefix='/school')
-    app.register_blueprint(payment, url_prefix='/payment')
-    app.register_blueprint(manager, url_prefix='/manager')
-
-    with app.app_context():
-        db.create_all()
+with app.app_context():
+    db.create_all()
 
 
-    @login_manager.user_loader
-    def load_user(member_id):
-        return db.get_or_404(Member,member_id)
+@login_manager.user_loader
+def load_user(member_id):
+    return db.get_or_404(Member, member_id)
 
 
-
-
-    return app
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
