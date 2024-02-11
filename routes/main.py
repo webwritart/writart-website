1dcf3033bafc67d311cfebf77bf7e823996f4fc6
Shwetabh <webwritat@gmail.com>
2024-02-11 17:02:50 +0530

    test scheduled operation

--- routes/main.py
+++ routes/main.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 from werkzeug.security import generate_password_hash
 from flask import Blueprint, render_template
@@ -6,6 +7,7 @@ from models.tool import Tools
 from models.member import Member, Workshop, Role
 from flask_login import current_user
 import pandas as pd
+from operations.messenger import send_email_school
 
 
 main = Blueprint('main', __name__, static_folder='static', template_folder='templates')
@@ -13,23 +15,12 @@ main = Blueprint('main', __name__, static_folder='static', template_folder='temp
 
 @main.route('/')
 def home():
-    # df = pd.read_csv('role.csv')
-    # for n in range(len(df)):
-    #     name = df.name[n]
-    #     desc = df.description[n]
-    #     print(f"{name} --- {desc}")
-    #
-    #     entry = Role(
-    #         name=name,
-    #         description=desc
-    #     )
-    #     db.session.add(entry)
-    #     db.session.commit()
+    time_now = str(datetime.datetime.now()).split()[1]
+    time = time_now.split('.')[0]
+    scheduled_time = "17:07:00"
 
-    # member = db.session.query(Member).filter_by(id=1).first()
-    # role = db.session.query(Role).filter_by(name='admin').first()
-    # member.role.remove(role)
-    # db.session.commit()
+    if time == scheduled_time:
+        send_email_school('scheduled_operation_test', ['shwetabhartist@gmail.com'], 'scheduled operation test', '', '')
 
     admin = db.session.query(Role).filter_by(name='admin').one_or_none()
     return render_template('index.html', logged_in=current_user.is_authenticated, admin=admin)
