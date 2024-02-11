c4bf0fc66df1b7946de5e69c46d0a689309fc16f
Shwetabh <webwritat@gmail.com>
2024-02-11 18:33:44 +0530

    sqlite to postgresql

--- routes/main.py
+++ routes/main.py
@@ -15,12 +15,6 @@ main = Blueprint('main', __name__, static_folder='static', template_folder='temp
 
 @main.route('/')
 def home():
-    time_now = str(datetime.datetime.now()).split()[1]
-    time = time_now.split('.')[0]
-    scheduled_time = "17:07:00"
-
-    if time == scheduled_time:
-        send_email_school('scheduled_operation_test', ['shwetabhartist@gmail.com'], 'scheduled operation test', '', '')
 
     admin = db.session.query(Role).filter_by(name='admin').one_or_none()
     return render_template('index.html', logged_in=current_user.is_authenticated, admin=admin)
