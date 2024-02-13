b195f63085e427c9b80d973550b9901ea9ce8d65
Shwetabh <webwritat@gmail.com>
2024-02-13 14:07:45 +0530

    fix cron

--- cron_jobs/test_cron.py
+++ cron_jobs/test_cron.py
@@ -1,22 +1,4 @@
-import smtplib
-from email.message import EmailMessage
+from operations.messenger import send_email_school
 
+send_email_school('cron test 111', ['shwetabhartist@gmail.com'], 'test for cron', '', '')
 
-def send_mail(subject, mail_message):
-    sender = 'shwetabh@writart.com'
-    password = 'isrgjexqhbjkqftr'
-
-    receiver = 'shwetabhartist@gmail.com'
-    message = mail_message
-    msg = EmailMessage()
-    msg['subject'] = subject
-    msg['From'] = sender
-    msg['To'] = receiver
-    msg.set_content(message)
-
-    with smtplib.SMTP_SSL('smtp.gmail.com') as smtp:
-        smtp.login(sender, password)
-        smtp.send_message(msg)
-
-
-send_mail('Test cron job', 'Testing cron job ubuntu user writart')
