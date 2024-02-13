56642a3728cdb3913b466669349f3b2fccb69705
Shwetabh <webwritat@gmail.com>
2024-02-13 13:51:19 +0530

    cron job script without module import

--- cron_jobs/test_cron.py
+++ cron_jobs/test_cron.py
@@ -1,2 +1,22 @@
-from messenger import send_email_school
-send_email_school('Test cron job', ['shwetabhartist@gmail.com'], 'Test cron job ubuntu', '', '')
+import smtplib
+from email.message import EmailMessage
+
+
+def send_mail(subject, mail_message):
+    sender = 'shwetabh@writart.com'
+    password = 'isrgjexqhbjkqftr'
+
+    receiver = 'shwetabhartist@gmail.com'
+    message = mail_message
+    msg = EmailMessage()
+    msg['subject'] = subject
+    msg['From'] = sender
+    msg['To'] = receiver
+    msg.set_content(message)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com') as smtp:
+        smtp.login(sender, password)
+        smtp.send_message(msg)
+
+
+send_mail('Test cron job', 'Testing cron job ubuntu user writart')
