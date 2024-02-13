534f88c13585a3dc5d810c55e9e1a51aa4c2ab4f
Shwetabh <webwritat@gmail.com>
2024-02-13 13:38:05 +0530

    fix import module cron

---
+++ cron_jobs/test_cron.py
@@ -0,0 +1,2 @@
+from messenger import send_email_school
+send_email_school('Test cron job', ['shwetabhartist@gmail.com'], 'Test cron job ubuntu', '', '')
--- routes/test_cron.py
+++
@@ -1,3 +0,0 @@
-import operations.messenger as m
-
-m.send_email_school('Test cron job', ['shwetabhartist@gmail.com'], 'Test cron job ubuntu', '', '')
