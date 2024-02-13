245be3d7fcd72bbdf1e7e87cc205ea4e77a5a377
Shwetabh <webwritat@gmail.com>
2024-02-13 13:16:34 +0530

    fix import cron job

--- cron_jobs/test_cron.py
+++ cron_jobs/test_cron.py
@@ -1,3 +1,3 @@
-from operations.messenger import send_email_school
+from ..operations.messenger import send_email_school
 
 send_email_school('Test cron job', ['shwetabhartist@gmail.com'], 'Test cron job ubuntu', '', '')
