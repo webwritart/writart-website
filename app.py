6d85f912661cc9869e0cc4d2d6fef92591402f6c
Shwetabh <webwritat@gmail.com>
2025-05-08 10:23:18 +0530

    studio page progress 1

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.environ.get('APP_SECRET')
 # app.secret_key = 'giehgeriogn94tgih*H()g94t9hg'
-# app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///writart.db"
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///writart.db"
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
 app.config["MAIL_SERVER"] = 'smtp.gmail.com'
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USERNAME"] = os.environ.get('MAIL_USERNAME')
@@ -70,5 +70,5 @@ def load_user(member_id):
 # ---------------------------------------------------------------------------------------------------------- #
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-    # app.run(debug=True)
+    # app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
