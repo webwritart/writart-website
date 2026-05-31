ae8bc44623cc70c1c56c8afa88890594a6df1826
Shwetabh <webwritat@gmail.com>
2026-05-31 20:19:44 +0530

    db test

--- models/member.py
+++ models/member.py
@@ -107,7 +107,7 @@ class Workshop(db.Model):
     __tablename__ = 'workshop'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(10))
+    name = db.Column(db.String(100))
     topic = db.Column(db.String(100))
     date = db.Column(db.String(50))
     time = db.Column(db.String(50))
