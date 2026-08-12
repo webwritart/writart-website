00e2da1a9208f67d82b18e07e1b3c4cf3ad7091b
Shwetabh <webwritat@gmail.com>
2026-08-12 18:29:43 +0530

    fix

--- app.py
+++ app.py
@@ -98,6 +98,6 @@ if __name__ == '__main__':
     # scheduler.init_app(app)
     # scheduler.add_job(id='my_task', func=task, trigger='interval', seconds=5)
     # # scheduler.start()
-    # app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
     # app.run(debug=True)
 
