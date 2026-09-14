f65c2e6ebcf964dd1838b45ec40cbb667d596fee
Shwetabh <webwritat@gmail.com>
2026-09-14 14:33:14 +0530

    Do not show login page if already logged in

--- routes/account.py
+++ routes/account.py
@@ -2001,130 +2001,134 @@ def register():
 
 @account.route('/login', methods=['GET', 'POST'])
 def login():
-    num_list = []
-    raw_num_list = []
-    result = db.session.query(Member)
-    if request.args.get('instruction'):
-        instruction = request.args.get('instruction')
+    if current_user.is_authenticated:
+        admin = db.session.query(Role).filter_by(name='admin').one_or_none()
+        return redirect(url_for('account.main_dashboard', logged_in=current_user.is_authenticated, current_year=current_year, admin=admin))
     else:
-        instruction = 'Login'
-    for user in result:
-        num = user.phone
-        raw_num_list.append(num)
-        if len(num) == 10:
-            user_no = f"91{num}"
-        elif len(num) == 11 and num[0] == '0' or '+':
-            user_no = f'91{num[1:]}'
-        elif len(num) == 12 and num[:2] == '91':
-            user_no = num
-        elif len(num) == 13 and num[:3] == '+91':
-            user_no = num[3:]
+        num_list = []
+        raw_num_list = []
+        result = db.session.query(Member)
+        if request.args.get('instruction'):
+            instruction = request.args.get('instruction')
         else:
-            user_no = num
-        num_list.append(user_no)
-    if request.method == 'POST':
-        if request.form.get('submit') == 'update_details':
-            pwd = request.form.get('password2')
-            retype_pwd = request.form.get('retype-password2')
-            if pwd != retype_pwd:
-                flash("Retyped password did not match! Please try again.", "error")
-                return render_template('update_account.html')
-            hash_and_salted_password = generate_password_hash(
-                pwd,
-                method='pbkdf2:sha256',
-                salt_length=8
-            )
-            current_user.password = hash_and_salted_password
-            current_user.sex = request.form.get('sex')
-            date_ = request.form.get('date')
-            if len(date_) < 2:
-                date_ = "0" + date_
-            month = request.form.get('month')
-            if len(month) < 2:
-                month = "0" + month
-            year = request.form.get('year')
-            current_user.dob = f"{year}-{month}-{date_}"
-            current_user.profession = request.form.get('profession')
-            current_user.state = request.form.get('state')
-            db.session.commit()
-            return redirect(url_for('account.home'))
-        if request.form.get('submit') == 'login':
-            data = request.form.get('email-phone')
-            if '@' in data:
-                email = data
-                result = db.session.execute(db.select(Member).where(Member.email == email))
-                user = result.scalar()
+            instruction = 'Login'
+        for user in result:
+            num = user.phone
+            raw_num_list.append(num)
+            if len(num) == 10:
+                user_no = f"91{num}"
+            elif len(num) == 11 and num[0] == '0' or '+':
+                user_no = f'91{num[1:]}'
+            elif len(num) == 12 and num[:2] == '91':
+                user_no = num
+            elif len(num) == 13 and num[:3] == '+91':
+                user_no = num[3:]
             else:
-                user_phone = ''
-                ph = data
-                if len(ph) == 10:
-                    phone = f"91{ph}"
-                elif len(ph) == 11 and ph[0] == '0' or '+':
-                    phone = f'91{ph[1:]}'
-                elif len(ph) == 12 and ph[:2] == '91':
-                    phone = ph
-                elif len(ph) == 13 and ph[:3] == '+91':
-                    phone = ph[3:]
+                user_no = num
+            num_list.append(user_no)
+        if request.method == 'POST':
+            if request.form.get('submit') == 'update_details':
+                pwd = request.form.get('password2')
+                retype_pwd = request.form.get('retype-password2')
+                if pwd != retype_pwd:
+                    flash("Retyped password did not match! Please try again.", "error")
+                    return render_template('update_account.html')
+                hash_and_salted_password = generate_password_hash(
+                    pwd,
+                    method='pbkdf2:sha256',
+                    salt_length=8
+                )
+                current_user.password = hash_and_salted_password
+                current_user.sex = request.form.get('sex')
+                date_ = request.form.get('date')
+                if len(date_) < 2:
+                    date_ = "0" + date_
+                month = request.form.get('month')
+                if len(month) < 2:
+                    month = "0" + month
+                year = request.form.get('year')
+                current_user.dob = f"{year}-{month}-{date_}"
+                current_user.profession = request.form.get('profession')
+                current_user.state = request.form.get('state')
+                db.session.commit()
+                return redirect(url_for('account.home'))
+            if request.form.get('submit') == 'login':
+                data = request.form.get('email-phone')
+                if '@' in data:
+                    email = data
+                    result = db.session.execute(db.select(Member).where(Member.email == email))
+                    user = result.scalar()
                 else:
-                    phone = ph
-                if phone in num_list:
-                    index = num_list.index(phone)
-                    user_phone = raw_num_list[index]
-                result = db.session.execute(db.select(Member).where(Member.phone == user_phone))
-                user = result.scalar()
-            password = request.form.get('password')
-
-            # Email or Phone doesn't exist or password incorrect:
-            if not user:
-                flash("That Email or Phone does not exist, please try again.", category="error")
-            elif not check_password_hash(user.password, password):
-                flash('Password incorrect, please try again.', category='error')
-            else:
-                login_user(user)
-                student = db.session.query(Role).filter_by(name='student').scalar()
-                admin = db.session.query(Role).filter_by(name='admin').scalar()
-                instructor = db.session.query(Role).filter_by(name='instructor').scalar()
-                artist = db.session.query(Role).filter_by(name='artist').scalar()
-                youtube_img_creator = db.session.query(Role).filter_by(name='youtube_img_creator').scalar()
-
-                user_roles = current_user.role
-                session['logged_in'] = True
-                if session.get('url') == url_for('manager.youtube_manager'):
-                    return redirect(url_for('manager.youtube_manager'))
-                if session.get('url') == url_for('school.course'):
-                    return redirect(url_for('school.course'))
-                if session.get('url') == url_for('school.classroom'):
-                    return redirect(url_for('school.classroom'))
-                if session.get('url') == url_for('school.enroll'):
-                    return redirect(url_for('school.enroll'))
-                if len(user_roles) > 1:
-                    return redirect(url_for('account.main_dashboard'))
-                else:
-                    if youtube_img_creator in user_roles and len(user_roles) < 2:
-                        return redirect(url_for('youtube.home'))
-                    if student in user_roles and len(user_roles) < 2:
-                        return redirect(url_for('account.student_dashboard'))
-                    if admin in user_roles:
-                        return redirect(url_for('manager.home'))
-                    if instructor in user_roles and len(user_roles) < 2:
-                        return redirect(url_for('account.instructor_dashboard'))
-                    if artist in user_roles and len(user_roles) < 2:
-                        return redirect(url_for('account.artist_dashboard'))
-                    if not current_user.sex or current_user.sex == '':
-                        return render_template('update_account.html')
-                    if request.form.get('prev-page') == 'enroll':
-                        flash("You are successfully logged in. Now proceed to enroll", "success")
-                        return redirect(url_for('payment.home'))
-                    if request.form.get('prev-page') == 'change-password':
-                        flash("You are successfully logged in. Now proceed to change password", "success")
-                        return redirect(url_for('account.change_password'))
-                    if 'url' in session:
-                        return redirect(session['url'])
+                    user_phone = ''
+                    ph = data
+                    if len(ph) == 10:
+                        phone = f"91{ph}"
+                    elif len(ph) == 11 and ph[0] == '0' or '+':
+                        phone = f'91{ph[1:]}'
+                    elif len(ph) == 12 and ph[:2] == '91':
+                        phone = ph
+                    elif len(ph) == 13 and ph[:3] == '+91':
+                        phone = ph[3:]
                     else:
-                        return redirect(url_for('main.home'))
-                # return redirect(url_for('account.home', name=current_user.name.split()[0]))
+                        phone = ph
+                    if phone in num_list:
+                        index = num_list.index(phone)
+                        user_phone = raw_num_list[index]
+                    result = db.session.execute(db.select(Member).where(Member.phone == user_phone))
+                    user = result.scalar()
+                password = request.form.get('password')
 
-    return render_template("login.html", instruction=instruction, current_year=current_year)
+                # Email or Phone doesn't exist or password incorrect:
+                if not user:
+                    flash("That Email or Phone does not exist, please try again.", category="error")
+                elif not check_password_hash(user.password, password):
+                    flash('Password incorrect, please try again.', category='error')
+                else:
+                    login_user(user)
+                    student = db.session.query(Role).filter_by(name='student').scalar()
+                    admin = db.session.query(Role).filter_by(name='admin').scalar()
+                    instructor = db.session.query(Role).filter_by(name='instructor').scalar()
+                    artist = db.session.query(Role).filter_by(name='artist').scalar()
+                    youtube_img_creator = db.session.query(Role).filter_by(name='youtube_img_creator').scalar()
+
+                    user_roles = current_user.role
+                    session['logged_in'] = True
+                    if session.get('url') == url_for('manager.youtube_manager'):
+                        return redirect(url_for('manager.youtube_manager'))
+                    if session.get('url') == url_for('school.course'):
+                        return redirect(url_for('school.course'))
+                    if session.get('url') == url_for('school.classroom'):
+                        return redirect(url_for('school.classroom'))
+                    if session.get('url') == url_for('school.enroll'):
+                        return redirect(url_for('school.enroll'))
+                    if len(user_roles) > 1:
+                        return redirect(url_for('account.main_dashboard'))
+                    else:
+                        if youtube_img_creator in user_roles and len(user_roles) < 2:
+                            return redirect(url_for('youtube.home'))
+                        if student in user_roles and len(user_roles) < 2:
+                            return redirect(url_for('account.student_dashboard'))
+                        if admin in user_roles:
+                            return redirect(url_for('manager.home'))
+                        if instructor in user_roles and len(user_roles) < 2:
+                            return redirect(url_for('account.instructor_dashboard'))
+                        if artist in user_roles and len(user_roles) < 2:
+                            return redirect(url_for('account.artist_dashboard'))
+                        if not current_user.sex or current_user.sex == '':
+                            return render_template('update_account.html')
+                        if request.form.get('prev-page') == 'enroll':
+                            flash("You are successfully logged in. Now proceed to enroll", "success")
+                            return redirect(url_for('payment.home'))
+                        if request.form.get('prev-page') == 'change-password':
+                            flash("You are successfully logged in. Now proceed to change password", "success")
+                            return redirect(url_for('account.change_password'))
+                        if 'url' in session:
+                            return redirect(session['url'])
+                        else:
+                            return redirect(url_for('main.home'))
+                    # return redirect(url_for('account.home', name=current_user.name.split()[0]))
+
+        return render_template("login.html", instruction=instruction, current_year=current_year)
 
 
 @account.route('/forgot_password', methods=['GET', 'POST'])
