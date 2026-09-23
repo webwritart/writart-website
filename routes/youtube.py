f124fc3dc897cad504abed548a3fb72dd799d84d
Shwetabh <webwritat@gmail.com>
2026-09-24 02:31:00 +0530

    fix

--- routes/youtube.py
+++ routes/youtube.py
@@ -162,7 +162,7 @@ def home():
                         current_creatives_upload_scene_shot_tuple = find_missing_creative(current_video)
                         if not current_creatives_upload_scene_shot_tuple:
                             if len(current_video.storyboard_scenes) > 0:
-                                last_scene = [a.scene for a in video.storyboard_scenes if a.scene == str(max([int(a.scene) for a in video.storyboard_scenes]))][0]
+                                last_scene = [a.scene for a in video.storyboard_scenes if a.scene == str(max([int(a.scene) for a in current_video.storyboard_scenes]))][0]
                                 last_shot = [a.shot for a in db.session.query(YoutubeVideoStoryboardScene).filter_by(scene=last_scene).one_or_none().shots if a.shot == str(max([int(a.shot) for a in db.session.query(YoutubeVideoStoryboardScene).filter_by(scene=last_scene).one_or_none().shots]))][0]
                                 current_creatives_upload_scene_shot_tuple = (last_scene, last_shot)
                             
@@ -184,7 +184,7 @@ def home():
                         current_creatives_upload_scene_shot_tuple = find_missing_creative(first_video)
                         if not current_creatives_upload_scene_shot_tuple:
                             if len(first_video.storyboard_scenes) > 0:
-                                last_scene = [a.scene for a in video.storyboard_scenes if a.scene == str(max([int(a.scene) for a in video.storyboard_scenes]))][0]
+                                last_scene = [a.scene for a in video.storyboard_scenes if a.scene == str(max([int(a.scene) for a in first_video.storyboard_scenes]))][0]
                                 last_shot = [a.shot for a in db.session.query(YoutubeVideoStoryboardScene).filter_by(scene=last_scene).one_or_none().shots if a.shot == str(max([int(a.shot) for a in db.session.query(YoutubeVideoStoryboardScene).filter_by(scene=last_scene).one_or_none().shots]))][0]
                                 current_creatives_upload_scene_shot_data_tuple = (last_scene, last_shot)
                         
